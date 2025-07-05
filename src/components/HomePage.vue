c51f79950bb6195a1b709e3a49d3631593640f46
Bogdan <zarechnyy00@internet.ru>
2025-07-05 16:26:26 +0300

    create component Project Statistic

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,6 +3,7 @@ import HeaderHome from './HeaderHome.vue'
 import OfferHome from './OfferHome.vue'
 import ProjectOpportunities from './ProjectOpportunities.vue'
 import HowItWorksHome from './HowItWorksHome.vue'
+import ProjectStatistic from './ProjectStatistic.vue'
 </script>
 
 <template>
@@ -10,6 +11,7 @@ import HowItWorksHome from './HowItWorksHome.vue'
   <OfferHome />
   <ProjectOpportunities />
   <HowItWorksHome />
+  <ProjectStatistic />
 </template>
 
 <style scoped></style>
